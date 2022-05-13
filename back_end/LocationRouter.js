4fb0e91e8e3faf60fc896cf5cd1af7c218f67ed4
AlexandraIosif7 <iosifalexandra7@yahoo.com>
2022-05-13 19:14:19 +0300

    Adding second component

--- back_end/LocationRouter.js
+++ back_end/LocationRouter.js
@@ -28,8 +28,16 @@ router.post('/add', async(req,res)=>{
     try{
         const newLocation ={
             origin: req.body.origin,
-            destination: req.body.destination
+            destination: req.body.destination,
+            distance: req.body.distance,
+            duration: req.body.duration
         };
+
+        if (!origin || !destination || !distance || !duration) {
+            return res.status(400).json({
+                error: "All fields are required",
+            });
+        }
         const response = await database.collection("locations").add(newLocation);
         res.send(response);
 
@@ -37,5 +45,29 @@ router.post('/add', async(req,res)=>{
         res.status(400).send(error.message);
     }
 });
+
+router.get('/:place', async (req,res)=>{
+    try{
+
+        const locations = database.collection("locations");
+        const data = await locations.get();
+        const locationsArray = [];
+        if(data.empty){
+            res.status(404).send('No location found!');
+        }
+        else{
+            data.forEach( doc =>{
+                if(doc.data().destination == req.params.place || doc.data().origin == req.params.place)
+                    locationsArray.push(doc.data());
+            });
+           res.send(locationsArray);
+        }
+    }
+    catch(error){
+        res.status(400).send(error.message);
+    }
+});
+
+
 
 module.exports = router;
